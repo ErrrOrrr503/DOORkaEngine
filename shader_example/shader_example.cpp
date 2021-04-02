75f8294d4817a579fd4d822f1928d1f17f8d88b0
teldufalsari <idunaev71@gmail.com>
2021-04-02 22:19:29 +0000

    Added vertical camera inclination and perspective projection

--- shader_example/shader_example.cpp
+++ shader_example/shader_example.cpp
@@ -20,9 +20,10 @@ glm::vec3 g_camera_pos(0.0f, 0.0f, 0.1f);
 glm::vec3 g_camera_direction(0.0f, 0.0f, -1.0f);
 glm::vec3 g_camera_up(0.0f, 1.0f, 0.0f);
 float g_camera_angle = 0.0f;
+float g_pitch_angle = 0.0f;
 // Camera velocity in unit/sec
-float g_camera_velocity = 0.5f;
-float g_angular_velocity = 0.5f;
+float g_camera_velocity = 2.0f;
+float g_angular_velocity = 2.0f;
 // Timing variables
 float g_elapsed_time = 0.0f;
 float g_last_frame = 0.0f;
@@ -30,10 +31,11 @@ float g_last_frame = 0.0f;
 // Shader source code (maybe it's a good idea to write in into a separate file and #include it)
 const GLchar* vshaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
-"uniform mat4 view;"
+"uniform mat4 view;\n"
+"uniform mat4 perspective;\n"
 "void main()\n"
 "{\n"
-"    gl_Position = view * vec4(aPos, 1.0);\n"
+"    gl_Position = perspective * view * vec4(aPos, 1.0);\n"
 "}\n\0";
 const GLchar* fshaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
@@ -56,9 +58,11 @@ void processInput(GLFWwindow *window)
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        g_camera_pos += g_camera_velocity * g_elapsed_time * g_camera_direction;
+        g_camera_pos += g_camera_velocity * g_elapsed_time
+        * glm::vec3(g_camera_direction.x, 0.0f, g_camera_direction.z);
     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-        g_camera_pos -= g_camera_velocity * g_elapsed_time * g_camera_direction;
+        g_camera_pos -= g_camera_velocity * g_elapsed_time
+        * glm::vec3(g_camera_direction.x, 0.0f, g_camera_direction.z);;
     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
         g_camera_pos -= glm::normalize(glm::cross(g_camera_direction, g_camera_up)) 
         * g_camera_velocity * g_elapsed_time;
@@ -67,16 +71,39 @@ void processInput(GLFWwindow *window)
         * g_camera_velocity * g_elapsed_time;
     if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
         g_camera_angle -= g_angular_velocity * g_elapsed_time;
-        if (g_camera_angle < -M_PI)
+        if (g_camera_angle < -M_PI + 0.001f)
             g_camera_angle += M_PI + M_PI;
-        g_camera_direction = glm::vec3(sinf(g_camera_angle), 0.0f, -cosf(g_camera_angle));
+        g_camera_direction = glm::vec3(
+            sinf(g_camera_angle) * cosf(g_pitch_angle),
+            sinf(g_pitch_angle),
+            -cosf(g_camera_angle) * cosf(g_pitch_angle));
     }
     if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
         g_camera_angle += g_angular_velocity * g_elapsed_time;
-        if (g_camera_angle > M_PI)
+        if (g_camera_angle > M_PI - 0.001f)
             g_camera_angle -= M_PI + M_PI;
-        g_camera_direction = glm::vec3(sinf(g_camera_angle), 0.0f, -cosf(g_camera_angle));
+        g_camera_direction = glm::vec3(
+            sinf(g_camera_angle) * cosf(g_pitch_angle),
+            sinf(g_pitch_angle),
+            -cosf(g_camera_angle) * cosf(g_pitch_angle));
     }
+    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+        if (g_pitch_angle < M_PI_2)
+            g_pitch_angle += g_angular_velocity * g_elapsed_time;
+        g_camera_direction = glm::vec3(
+            sinf(g_camera_angle) * cosf(g_pitch_angle),
+            sinf(g_pitch_angle),
+            -cosf(g_camera_angle) * cosf(g_pitch_angle));
+    }
+    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+        if (g_pitch_angle > -M_PI_2)
+            g_pitch_angle -= g_angular_velocity * g_elapsed_time;
+        g_camera_direction = glm::vec3(
+            sinf(g_camera_angle) * cosf(g_pitch_angle),
+            sinf(g_pitch_angle),
+            -cosf(g_camera_angle) * cosf(g_pitch_angle));
+    }
+
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
@@ -143,6 +170,7 @@ int main()
     sphere sfera(0.0, 0.0, 0.0, 1.0, 98, 100);
     init();
     glm::mat4 view;
+    glm::mat4 perspective;
 
     // Starting render loop
     while(!glfwWindowShouldClose(window)) {
@@ -151,12 +179,16 @@ int main()
         // Getting the location of uniform variable with color vector
         GLint colorVarLocation = glGetUniformLocation(shaderProgram, "requestedColor");
         GLint viewVarLocation = glGetUniformLocation(shaderProgram, "view");
+        GLint perspVarLocation = glGetUniformLocation(shaderProgram, "perspective");
         glUseProgram(shaderProgram);
         // Setting color
         glUniform4f(colorVarLocation, 1.0f, 1.0f, 0.1f, 1.0f);
         // Setting view matrix
         view = glm::lookAt(g_camera_pos, g_camera_pos + g_camera_direction, g_camera_up);
         glUniformMatrix4fv(viewVarLocation, 1, GL_FALSE, glm::value_ptr(view));
+        // Setting perspective projection matrix
+        perspective = glm::perspective<float>(glm::radians(45.0f), 1.0f, 0.1f, 100.0f);
+        glUniformMatrix4fv(perspVarLocation, 1, GL_FALSE, glm::value_ptr(perspective));
         sfera.draw();
         glfwSwapBuffers(window);
         float cur_time = glfwGetTime();
