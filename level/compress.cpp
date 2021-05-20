c43e3072dd70bd224e4d61a69131f0e1eb441500
ErrrOrrr503 <egortit-33@yandex.ru>
2021-05-20 10:30:55 +0300

    refactor

--- level/compress.cpp
+++ level/compress.cpp
@@ -6,7 +6,7 @@ std::vector<pos_t> compress(const char* input, size_t input_size)
     size_t dict_size = 256;
     std::unordered_map<std::string, pos_t> dict;
     for (pos_t i = 0; i < dict_size; i++)
-        dict[std::string(1, i)] = i;
+        dict[std::string(1, (char) i)] = i;
     std::string buffer, bc;
     for (size_t i = 0; i < input_size; i++) {
         char c = input[i];
@@ -16,7 +16,7 @@ std::vector<pos_t> compress(const char* input, size_t input_size)
         } else {
             result.push_back(dict[buffer]);
             if (dict_size < TABLE_SIZE)
-                dict[bc] = dict_size++;
+                dict[bc] = (short unsigned) dict_size++;
             buffer = std::string(1, c);
         }
     }
@@ -33,15 +33,15 @@ int decompress(const pos_t* input, size_t input_size, char* result)
         return COMP_ERR_ALLOC;
     try {
         for (pos_t i = 0; i < dict_size; i++)
-            dict[i] = new std::string(1, i);
+            dict[i] = new std::string(1, (char) i);
     } catch(std::bad_alloc) {
         for (pos_t i = 0; i < dict_size; i++)
             delete dict[i];
         free(dict);
         return COMP_ERR_ALLOC;
     }
-    std::string w(1, input[0]);
-    result[0] = input[0];
+    std::string w(1, (char) input[0]);
+    result[0] = (char) input[0];
     size_t result_offset = 1;
     std::string buffer;
     for (size_t i = 1; i < input_size; i++) {
@@ -51,8 +51,8 @@ int decompress(const pos_t* input, size_t input_size, char* result)
         } else if (code == dict_size) {
             buffer = w + w[0];
         } else {
-            for (size_t i = 0; i < dict_size; i++)
-                delete dict[i];
+            for (size_t j = 0; j < dict_size; j++)
+                delete dict[j];
             free(dict);
             return COMP_ERR_DECODE;
         }
@@ -63,8 +63,8 @@ int decompress(const pos_t* input, size_t input_size, char* result)
                 dict[dict_size++] = new std::string(w + buffer[0]);
             }
         } catch(std::bad_alloc) {
-            for (pos_t i = 0; i < dict_size; i++)
-                delete dict[i];
+            for (pos_t j = 0; j < dict_size; j++)
+                delete dict[j];
             free(dict);
             return COMP_ERR_ALLOC;
         }
--- level/level_common.h
+++ level/level_common.h
@@ -87,13 +87,13 @@ struct level_fileheader {
 struct wall {
     wall ();
     wall (float new_x1, float new_y1, float new_x2, float new_y2);
-    wall (float new_x1, float new_y1, float new_x2, float new_y2, bool is_colored);
+    wall (float new_x1, float new_y1, float new_x2, float new_y2, bool flag_is_colored);
     float x1 = 0, y1 = 0, zlo1 = 0, zhi1 = DEF_WALL_HEIGHT;
     float x2 = 0, y2 = 0, zlo2 = 0, zhi2 = DEF_WALL_HEIGHT;
     float normal_x = 0, normal_y = 0;
     float color[3] = {DEF_WALL_COLOR_R, DEF_WALL_COLOR_G, DEF_WALL_COLOR_B};
     int32_t texture_index = -1;  // -1 no texture; 0-32767 texture index in
-    bool is_colored = false;
+    bool is_colored;
 };
 
 /*!
